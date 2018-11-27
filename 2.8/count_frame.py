0e0bc2a80789ab3b6c57b2dbf24147d0efc79142
forAllBright <pmzseu@gmail.com>
2018-11-28 00:36:24 +0800

    local creation

--- 2.8/count_frame.py
+++ 2.8/count_frame.py
@@ -17,7 +17,44 @@ def fib(n):
     else:
         return fib(n - 1) + fib(n - 2)
 
+#
+# fib = count_frames(fib)
+# print(fib(24))
+# print(fib.max_count)
 
-fib = count_frames(fib)
-print(fib(24))
-print(fib.max_count)
+
+############################################################################################################
+# memoization decorator
+
+def count(f):
+    def counted(*args):
+        counted.call_count += 1
+        return f(*args)
+
+    counted.call_count = 0
+    return counted
+
+def memoization(f):
+    cache = {}
+    def inner(n):
+        nonlocal cache
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n]=f(n)
+            return cache[n]
+    return inner
+
+# fibb = memoization(fib)
+# print(fibb(10))
+#
+# fib = count_frames(fib)
+# fibbb = memoization(fib)
+# print(fibbb(24))
+# print("**{}**".format(fib.max_count))
+
+counted_fib = count(fib)
+fib  = memoization(counted_fib)
+## 要是需要将装饰后的函数进行递归，必须将装饰（）后的函数重新赋值给原来函数名，其实装饰器已经做到这一点
+print(fib(19))
+print(counted_fib.call_count)
--- 2.8/pmz_test.py
+++ 2.8/pmz_test.py
@@ -4,7 +4,7 @@
 # @Last Modified by:   forAllBright
 # @Last Modified time: 2018-11-27 21:56:56
 ###########################################
-#在装饰器内部嵌套函数中调用外层传入函数 是装饰前原函数还是装饰后函数 问题
+# 在装饰器内部嵌套函数中调用外层传入函数 是装饰前原函数还是装饰后函数 问题
 ###########################################
 
 def count_frames(f):
@@ -26,12 +26,36 @@ def fib(n):
     else:
         return fib(n - 1) + fib(n - 2)
 
-# 注意左边的变量是 fi，这种情况下在“调用位置1”处的 f 是原来的 fib
-fi = count_frames(fib)
-print(fi(24))   # 75025
-print(fi.max_count) # 1
 
-#左边的变量是 fib，这种情况下，在 counted的词法作用域中，fib 是装饰后的函数，因此在“调用位置1”的 f 是装饰后的 fib
-fib = count_frames(fib)
-print(fib(24))   # 75025
-print(fib.max_count) # 24
+# 注意左边的变量是 fi，这种情况下在“调用位置1”处的 f 是原来的 fib
+# fi = count_frames(fib)
+# print(fi(24))  # 75025
+# print(fi.max_count)  # 1
+#
+# # 左边的变量是 fib，这种情况下，在 counted的词法作用域中，fib 是装饰后的函数，因此在“调用位置1”的 f 是装饰后的 fib
+# fib = count_frames(fib)
+# print(fib(24))  # 75025
+# print(fib.max_count)  # 24
+
+def fib(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+def count(f):
+    def counted(*args):
+        counted.call_count += 1
+        return f(*args)
+
+    counted.call_count = 0
+    return counted
+
+
+fi = count(fib)
+print(fi(19)) #6765
+print(fi.call_count) #1
+
+fib = count(fib)
+print(fib(19)) #6765
+print(fib.call_count) #13529
